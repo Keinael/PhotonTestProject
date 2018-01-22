d682b4945dc1e330fff24f07305f562fe27de110
Keinael <usenko.ua@gmail.com>
2018-01-22 11:38:15 +0200

    Part 3 Game Scenes done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class GameManager : MonoBehaviour 
+    {
+
+        #region Photon Messages
+
+        /// <summary>
+        /// Called when the local player left the room. 
+        /// </summary>
+        public void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+    }
+}
+
+
